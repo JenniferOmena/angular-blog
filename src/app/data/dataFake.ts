dee9b289e63a4254c47fada3e8635af6ab5b4252
Jennifer Omena <jenniferomena11@outlook.com>
2024-02-24 18:23:47 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Curabitur faucibus",
+        "description":"Vivamus augue felis, mollis vitae ante et, placerat sollicitudin ligula. Fusce id enim molestie, varius odio quis, faucibus est. In lectus risus, venenatis vel massa fringilla, sagittis ullamcorper metus.",
+        "photoCover":"https://st4.depositphotos.com/17828278/24401/v/450/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available.jpg"
+    },
+    
+    {
+        "id":"2",
+        "title":"Phasellus convallis",
+        "description":"Fusce scelerisque convallis est, in interdum felis fermentum at. Phasellus et est eget ante finibus vestibulum et vulputate libero. Donec ultrices mauris quis pretium vulputate. Proin maximus consectetur est, in eleifend lectus blandit vitae. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus non nibh sodales, condimentum justo vitae, auctor turpis. Proin dapibus interdum est id feugiat. Etiam quis velit eleifend, consectetur velit sed, condimentum lorem. Nunc in luctus massa. Nunc a quam vulputate, dignissim magna id, egestas arcu.",
+        "photoCover":"https://st4.depositphotos.com/17828278/24401/v/450/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available.jpg"
+    },
+    
+    {
+        "id":"3",
+        "title":"Vivamus tincidunt",
+        "description":"Ut pulvinar nunc et venenatis egestas. Donec laoreet eget enim ac cursus. Vivamus sit amet elit sed ipsum aliquam pharetra. Praesent finibus sollicitudin nibh vel lacinia. Aliquam purus libero, gravida sit amet tellus nec, accumsan sollicitudin quam. Vivamus mattis nisl a blandit consequat. Vestibulum nec dapibus nunc. Mauris finibus magna id luctus dignissim.",
+        "photoCover":"https://st4.depositphotos.com/17828278/24401/v/450/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available.jpg"
+    },
+
+    {
+        "id":"4",
+        "title":"Nulla facilisi",
+        "description":"Aliquam magna diam, varius sed ligula a, consequat facilisis risus. Aliquam aliquet sem non magna bibendum, nec rutrum lectus laoreet. Phasellus fermentum odio at venenatis tempor. Nunc vehicula facilisis quam, vitae feugiat odio semper sit amet. Quisque pulvinar, ligula sed dignissim sagittis, ligula ante laoreet purus, egestas scelerisque ipsum tellus sed quam. Fusce vel dolor viverra, imperdiet libero eget, dapibus magna. Phasellus vel varius dolor.",
+        "photoCover":"ahttps://st4.depositphotos.com/17828278/24401/v/450/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available.jpg"
+    }
+]
